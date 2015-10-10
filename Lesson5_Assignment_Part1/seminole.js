d2195c331274ae9670cbf4b68846465fb6860b48
kritty7 <kmh12f@my.fsu.edu>
2015-10-10 13:06:20 +0000

    Lesson 5 Assignment - FINAL COMMIT!

--- Lesson5_Assignment_Part1/seminole.js
+++ Lesson5_Assignment_Part1/seminole.js
@@ -1,16 +1,31 @@
 // Program Name: Seminole Trojans
+ 
 // Program topics: Variables, Functions, Arrays, Loops
+ 
 // Author: Kristen Howard
+ 
 // Date: October 10, 2015
+
 // Filename: seminole.js
 
-//    global variables
-var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]'
+//global variables
+var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday",
+                  "Thursday", "Friday", "Saturday"];
 // global variables
-var opponents = ["Lightning", "Combines", "Combines", "Combines", "Lightning", "Lightning", "Lightning", "Lightning", "Barn Raisers", "Barn Raisers", "BArn Raisers", "Sodbusters", "Sodbusters", "Sodbusters", "Sodbusters", "(off)", "River Riders", "Rivers Riders", "River Riders", "Big Dippers", "Big Dippers", "Big Dippers", "(off)", "Sodbusters", "Sodbusters", "Sodbusters", "Combines", "Combines", "Combines", "(off)", "(off)"];
+var opponents = ["Lightning", "Combines", "Combines",
+    "Combines", "Lightning", "Lightning", "Lightning", 
+    "Lightning", "Barn Raisers", "Barn Raisers", 
+    "Barn Raisers", "Sodbusters", "Sodbusters", "Sodbusters",
+    "Sodbusters", "(off)", "River Riders", "Rivers Riders", 
+    "River Riders", "Big Dippers", "Big Dippers", 
+    "Big Dippers", "(off)", "Sodbusters", "Sodbusters", 
+    "Sodbusters", "Combines", "Combines", "Combines", 
+    "(off)", "(off)"];
+
 // global variables
 var gameLocation =
     ["away", "away", "away", "away", "home", "home", "home", "home", "home", "home", "home", "away", "away", "away", "", "home", "home", "home", "home", "home", "", ""];
+
 // function to place daysOfWeek values in header row cells
 function addColumnHeaders() {
   var i = 0; 
@@ -19,6 +34,7 @@ function addColumnHeaders() {
     i++;
   } //end of while loop
 } //end of addColumnHeaders function
+
 //function to place day of month value in first p element
 //within each table data cell that has an id
 function addCalendarDates() {
@@ -31,11 +47,13 @@ function addCalendarDates() {
     i++;
   } while (i < 32);
 } //end of addCalendarDates function
+
 //This function will place the game information in the calendar
+//second p element in each table cell that has id
 function addGameInfo() {
   var paragraphs = "";
-  far (var i = 0; i < 31; i++) {
-    var date = i+1;
+  for (var i = 0; i < 31; i++) {
+    var date = i + 1;
     var tableCell = document.getElementById("08-" + date);
     paragraphs = tableCell.getElementsByTagName("p");
     paragraphs[1].innerHTML += opponents[i];
@@ -52,11 +70,24 @@ function addGameInfo() {
   }//end of the for loop
 }// end of the function
 
+//This statement will add the column headers to the page immedieately when the page loads in the browser window
+window.addEventsListener("load", addColumnHeaders, false); 
+
+//function to place daysOfWeek values in header row cells
+function addColumnHeaders() {
+  var i =0;
+  while (i < 7) {
+    document.getElementsByTagName("th")[i].innerHTML = daysOfWeek[i];
+    i++;
+  }//end of while loop
+}//end of add ColumnHeaders function
+
 //function to load the calendar content in the calendar when the page loads
 function setUpPage() {
   addColumnHeaders();
   addCalendarDates();
   addGameInfo();
 } //end of setUpPage function
-//This statement will add the column headers to the page immedieately when the page loads in the browser window
-window.addEventsListener("load", setUpPage(), false); 
+
+//This statment will add the column headers to page
+window.addEventListener("load", setUpPage(), false);
